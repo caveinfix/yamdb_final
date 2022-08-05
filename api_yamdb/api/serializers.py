c0e10aed350b08ea5bc289f201ecd91b96060058
Philipp Pyhonin <caveinfix@gmail.com>
2022-08-05 12:55:29 +0300

    ix

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -4,6 +4,7 @@ from django.utils.crypto import get_random_string
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from rest_framework.validators import UniqueTogetherValidator
+
 from reviews.models import Category, Comment, CustomUser, Genre, Review, Title
 
 
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -9,6 +9,7 @@ from rest_framework.generics import CreateAPIView
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+
 from reviews.models import Category, Comment, CustomUser, Genre, Review, Title
 
 from .filters import TitleFilter
